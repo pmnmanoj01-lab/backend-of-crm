b575435f0b654b54de7bc07ee655fff57d437eab
pmnmanoj01-lab <pmn.manoj01@gmail.com>
2025-12-05 13:16:07 +0530

    some validation on env and nodemodules

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const app = express();
 // Middleware
 const allowedOrigins = [
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://crm-six-blond.vercel.app/"
 ];
 
 app.use(cors({
